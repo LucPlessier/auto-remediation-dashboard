6f70e2203425d56f3803a74388537b7ac242569e
LucPlessier <lucplessier@Lucs-MBP.home>
2024-12-30 17:33:05 +0000

    Add comments to next.config.js to trigger deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Enable React strict mode for better development experience
   reactStrictMode: true,
+  // Use SWC minifier for better performance
   swcMinify: true,
 }
 
